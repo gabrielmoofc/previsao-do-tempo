74df24204c4cdabf8aa6ddd4557a6e28990febad
Gabriel Moreira <gabrielmmm25@gmail.com>
2023-01-28 09:31:56 -0300

    Layout da main content e aside
    - Recriado layout da main content
    
    - Editado layout da aside
    
    - Setado fonte globalmente no app.js (Estou procurando uma fonte que se enquadre mais  ao projeto)

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -43,49 +43,51 @@ export default function Home() {
         </div>
       </nav>
       <main id="mainContainer">
-        <div id="mainContent">
-          <div id="nomeCidade">
-            <h1>Dourados, MS, Brasil</h1>
-            <h3>12:30 PM</h3>
-          </div>
-          <div id="temperaturaContainer">
-            <div id="temperaturaContent">
-              <img id="nublado" src={nublado} />
-              <h1 id="txtTemperatura">37°C</h1>
+        <div id="mainLayout">
+          <div id="mainContent">
+            <div id="nomeCidade">
+              <h1>Dourados, MS, Brasil</h1>
+              <h3>12:30 PM</h3>
             </div>
-            <div id="mensagemClimaContainer">
-              <h1>Muitas Nuvens</h1>
-              <h2>Sensação Térmica</h2>
+            <div id="temperaturaContainer">
+              <div id="temperaturaContent">
+                <img id="nublado" src={nublado} />
+                <h1 id="txtTemperatura">37°C</h1>
+              </div>
+              <div id="mensagemClimaContainer">
+                <h1>Muitas Nuvens</h1>
+                <h2>Sensação Térmica</h2>
+              </div>
             </div>
-          </div>
-          <div id="mensagemDiaContainer">
-            <div id="mensagemDia">
-              <p>
-                lorem loremloremv lorem lorem lorem lorem lorem lorem loremlorem
-                loremloremlorem lorem loremlorem lorem
-              </p>
+            <div id="mensagemDiaContainer">
+              <div id="mensagemDia">
+                <p>
+                  lorem loremloremv lorem lorem lorem lorem lorem lorem
+                  loremlorem loremloremlorem lorem loremlorem lorem
+                </p>
+              </div>
             </div>
-          </div>
-          <div id="informacoesContainer">
-            <div className="itemsInfo">
-              <p>QUALI. DO AR</p>
-              <p>37</p>
-            </div>
-            <div className="itemsInfo">
-              <p>VENTO</p>
-              <p>20km/h</p>
-            </div>
-            <div className="itemsInfo">
-              <p>ÚMIDADE</p>
-              <p>70%</p>
-            </div>
-            <div className="itemsInfo">
-              <p>PRESSÃO</p>
-              <p>1012 mb</p>
-            </div>
-            <div className="itemsInfo">
-              <p>VISIBILIDADE</p>
-              <p>5km/h</p>
+            <div id="informacoesContainer">
+              <div className="itemsInfo">
+                <p>QUALI. DO AR</p>
+                <p>37</p>
+              </div>
+              <div className="itemsInfo">
+                <p>VENTO</p>
+                <p>20km/h</p>
+              </div>
+              <div className="itemsInfo">
+                <p>ÚMIDADE</p>
+                <p>70%</p>
+              </div>
+              <div className="itemsInfo">
+                <p>PRESSÃO</p>
+                <p>1012 mb</p>
+              </div>
+              <div className="itemsInfo">
+                <p>VISIBILIDADE</p>
+                <p>5km/h</p>
+              </div>
             </div>
           </div>
         </div>
